7ed8c4b841c2acb6569e12ad5b2e0b01e178c34f
gasampaiosouza <gasampaio.souza@gmail.com>
2020-06-27 22:38:58 -0300

    starting chart

--- src/components/Main/Chart.tsx
+++ src/components/Main/Chart.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
 import style from './style.module.scss';
+import { Line } from 'react-chartjs-2';
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      // borderColor: 'rgba(75,192,192,1)',
+      // borderCapStyle: 'butt',
+      // borderDash: [],
+      // borderDashOffset: 0.0,
+      // borderJoinStyle: 'miter',
+      // pointBorderColor: 'rgba(75,192,192,1)',
+      // pointBackgroundColor: '#fff',
+      // pointBorderWidth: 1,
+      // pointHoverRadius: 5,
+      // pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      // pointHoverBorderColor: 'rgba(220,220,220,1)',
+      // pointHoverBorderWidth: 2,
+      // pointRadius: 1,
+      // pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40],
+    },
+  ],
+  options: {
+    tooltips: {
+      enabled: false,
+    },
+  },
+};
 
 const Chart = () => {
-  return <div className={style['chart--container']}>Chart</div>;
+  return (
+    <div className={style['chart--container']}>
+      <Line data={data} />
+    </div>
+  );
 };
 
 export default Chart;
